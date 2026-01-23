be21f1351156ba6f33d385235934f19d318b7e22
Astronomy <122727449+Astronomy20@users.noreply.github.com>
2026-01-23 08:51:49 +0100

    Add Background enum for character backgrounds

---
+++ src/main/java/net/astronomy/dnd/enums/Background.java
@@ -0,0 +1,17 @@
+package net.astronomy.dnd.enums;
+
+public enum Background {
+    ACOLYTE,
+    CHARLATAN,
+    CRIMINAL,
+    ENTERTAINER,
+    FOLK_HERO,
+    GUILD_ARTISAN,
+    HERMIT,
+    NOBLE,
+    OUTLANDER,
+    SAGE,
+    SAILOR,
+    SOLDIER,
+    URCHIN
+}
