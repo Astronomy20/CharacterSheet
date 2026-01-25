54ccee84944d3248bd4c2c0c70ec825748d78522
Matteo Dabbene <matteo07.dabbene@gmail.com>
2026-01-25 20:52:46 +0100

    Added languages. Updated enums with display names.

--- src/main/java/net/astronomy/dnd/enums/attributes/Alignment.java
+++ src/main/java/net/astronomy/dnd/enums/attributes/Alignment.java
@@ -1,13 +1,31 @@
 package net.astronomy.dnd.enums.attributes;
 
 public enum Alignment {
-    CHAOTIC_GOOD,
-    CHAOTIC_NEUTRAL,
-    CHAOTIC_EVIL,
-    NEUTRAL_GOOD,
-    TRUE_NEUTRAL,
-    NEUTRAL_EVIL,
-    LAWFUL_GOOD,
-    LAWFUL_NEUTRAL,
-    LAWFUL_EVIL
+
+    LAWFUL_GOOD("Lawful Good"),
+    NEUTRAL_GOOD("Neutral Good"),
+    CHAOTIC_GOOD("Chaotic Good"),
+
+    LAWFUL_NEUTRAL("Lawful Neutral"),
+    TRUE_NEUTRAL("True Neutral"),
+    CHAOTIC_NEUTRAL("Chaotic Neutral"),
+
+    LAWFUL_EVIL("Lawful Evil"),
+    NEUTRAL_EVIL("Neutral Evil"),
+    CHAOTIC_EVIL("Chaotic Evil");
+
+    private final String displayName;
+
+    Alignment(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
+    }
 }
--- src/main/java/net/astronomy/dnd/enums/attributes/Background.java
+++ src/main/java/net/astronomy/dnd/enums/attributes/Background.java
@@ -1,17 +1,33 @@
 package net.astronomy.dnd.enums.attributes;
 
 public enum Background {
-    ACOLYTE,
-    CHARLATAN,
-    CRIMINAL,
-    ENTERTAINER,
-    FOLK_HERO,
-    GUILD_ARTISAN,
-    HERMIT,
-    NOBLE,
-    OUTLANDER,
-    SAGE,
-    SAILOR,
-    SOLDIER,
-    URCHIN
+
+    ACOLYTE("Acolyte"),
+    CHARLATAN("Charlatan"),
+    CRIMINAL("Criminal"),
+    ENTERTAINER("Entertainer"),
+    FOLK_HERO("Folk Hero"),
+    GUILD_ARTISAN("Guild Artisan"),
+    HERMIT("Hermit"),
+    NOBLE("Noble"),
+    OUTLANDER("Outlander"),
+    SAGE("Sage"),
+    SAILOR("Sailor"),
+    SOLDIER("Soldier"),
+    URCHIN("Urchin");
+
+    private final String displayName;
+
+    Background(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
+    }
 }
